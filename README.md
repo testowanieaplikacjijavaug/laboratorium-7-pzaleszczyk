355e93db7287d52075dc49a9154798a9dc934af9
Suchy-Laptop <Suchy-Laptop@LAPTOP-BMKRSQB4>
2020-03-30 09:27:00 +0200

    Init

---
+++ README.md
@@ -0,0 +1,6 @@
+Lab7 - Pzaleszczyk
+
+![Java CI with Gradle](https://github.com/testowanieaplikacjijavaug/laboratorium-7-pzaleszczyk/workflows/Java%20CI%20with%20Gradle/badge.svg)
+
+
+[![codecov](https://codecov.io/gh/testowanieaplikacjijavaug/laboratorium-7-pzaleszczyk/branch/master/graph/badge.svg)](https://codecov.io/gh/testowanieaplikacjijavaug/laboratorium-7-pzaleszczyk)
---
+++ src/test/java/NoteTest.java
@@ -0,0 +1,40 @@
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.FileReader;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Matchers.startsWith;
+
+
+public class NoteTest {
+	
+	Note note;
+		
+//	public static Note of(final String name, final float note);
+//	public String getName();
+//	public float getNote();
+
+    @Test 
+    void trivialtest() {
+        assertTrue(true);
+    }
+    
+}
+
+
